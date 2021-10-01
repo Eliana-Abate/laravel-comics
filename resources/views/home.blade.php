a5508366d5a911be091a8e1e3381da860fb0272e
Eliana-Abate <eliana.abate@outlook.it>
2021-10-01 16:26:16 +0200

    added header.blade and scss files

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DC Comics</title>
+    <link rel="stylesheet" href="{{assets('css/app.css')}}">
+</head>
+<body>
+    @include('includes.header')
+    <h1>TEST</h1>
+    
+</body>
+</html>
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,22 @@
+<header>
+    <section id="main-header">
+        <div class="container h-100">
+            <div class="row h-100">
+
+                <div id="header-logo" class="col-40 row">
+                    <img src="" alt="dc-logo">
+                </div>
+
+                <div class="col-60">
+                    <nav>
+                        <ul class="row">
+                            <li>
+                                <a href="#">Comics</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div> 
+        </div>
+    </section>
+</header>
