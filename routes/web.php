63287722a53fd6f3c273e1a1b6223493b3acb72e
Eliana-Abate <eliana.abate@outlook.it>
2021-10-01 19:53:17 +0200

    added data in route via config

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $fumetti = config('comics');
+    return view('home', ['comics' => $fumetti]);
 })->name('home');
 
 Route::get('/comics', function () {
