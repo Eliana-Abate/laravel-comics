2a19f2bdd96003ffd34840865f835ae31c25e0e3
Eliana-Abate <eliana.abate@outlook.it>
2021-10-04 23:00:13 +0200

    added style to single comic

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -3,15 +3,26 @@
 @section('title') {{$comic['series']}} @endsection
 
 @section('content')
-    <div class="container-stretch">
+    <div class="container-stretch py-100">
         <div class="row">
             <div class="col-60">
-                <h1>{{$comic['title']}}</h1>
-                <div class="row">
-                    <div>{{$comic['price']}}</div>
-                    <div></div>
+                <h1 class="comic-title">{{$comic['title']}}</h1>
+                <div class="row green-table">
+                    <div class="col-65 bord-right">
+                        <div class="row">
+                            <h4>U.S. Price: </h4>
+                            <h4> {{$comic['price']}} </h4>
+                        </div>
+                        <div>
+                            <h4>AVAILABLE</h4>
+                        </div>
+                    </div>
+                    <div class="col-35">
+                       <h4>Check Availability</h4>
+                       <i class="fas fa-sort-down"></i>
+                    </div>
                 </div>
-                <p>{{$comic['description']}}</p>
+                <p class="comic-text">{{$comic['description']}}</p>
             </div>
             <div class="col-40">
                 <h4>ADVERTISEMENT</h4>
