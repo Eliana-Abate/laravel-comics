4397c20961b03e6ba9a2a94320c265a83d2752ce
Eliana-Abate <eliana.abate@outlook.it>
2021-10-05 00:22:08 +0200

    completed style of first row in comic.blade

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -3,36 +3,37 @@
 @section('title') {{$comic['series']}} @endsection
 
 @section('content')
-    <div class="container-stretch py-100">
-        <div class="row">
-            <div class="col-60">
+    <div class="container-stretch">
+        <div class="row pb-70">
+
+            <div class="col-65 row-2 pt-70">
                 <h1 class="comic-title">{{$comic['title']}}</h1>
                 <div class="row green-table">
-                    <div class="col-65 bord-right">
+                    <div class="col-65 bord-right row-1">
                         <div class="row">
-                            <h4>U.S. Price: </h4>
+                            <h4 class="light-text mg-right-10">U.S. Price: </h4>
                             <h4> {{$comic['price']}} </h4>
                         </div>
                         <div>
-                            <h4>AVAILABLE</h4>
+                            <h4 class="light-text">AVAILABLE</h4>
                         </div>
                     </div>
-                    <div class="col-35">
-                       <h4>Check Availability</h4>
+
+                    <div class="col-35 row-center">
+                       <h4 class="mg-right-10">Check Availability</h4>
                        <i class="fas fa-sort-down"></i>
                     </div>
                 </div>
                 <p class="comic-text">{{$comic['description']}}</p>
             </div>
-            <div class="col-40">
-                <h4>ADVERTISEMENT</h4>
+
+            <div class="col-35 special-row-4">
                 <figure>
+                    <h4>ADVERTISEMENT</h4>
                     <img src="{{asset('images/adv.jpg')}}" alt="adv">
                 </figure>
-
             </div>
-        </div>
-        
+        </div>  
     </div>
     
 @endsection
